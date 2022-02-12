deda6689b888d3e4486c30144340dbad3af79f48
Dhruvil Vyas <dhvyas@paypal.com>
2022-02-12 00:44:41 -0800

    Created the FeedItem struct.

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Dhruvil Vyas on 2/12/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: String
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
