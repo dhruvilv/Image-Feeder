22d715ec0a1ef73729b45b3da26343f4ed8ebd9e
Dhruvil Vyas <dhvyas@paypal.com>
2022-02-13 23:35:40 -0800

    Some formatting

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -12,22 +12,26 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
+        
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://www.a-url.com")!
         let (sut, client) = makeSUT(url: url)
+        
         sut.load()
+        
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://www.a-url.com")!
         let (sut, client) = makeSUT(url: url)
+        
         sut.load()
         sut.load()
-        XCTAssertEqual(client.requestedURLs.count, 2)
+        
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
